b94dddac2ec604a544bb3e1171221673780b1869
Andyvux <anhvuktnh@gmail.com>
2023-03-09 21:19:10 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+const overlay = document.querySelector("#overlay");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
